2c29b8da4398f74be565538772e636a5e429bacf
Xiaowu Chen <114638571@qq.com>
2014-12-04 22:52:46 +0800

    修改head

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 #网站信息
 WEB_URL='/'
 WEB_NAME='上帝De助手 的博客'
-WEB_SUBNAME='我不生产内容，我只是互联网的搬运工！'
+WEB_SUBNAME='我只生产内容，我不是互联网的搬运工！'
 WEB_TITLE='seo基础入门教程_seo入门学习_seo优化教程'
 WEB_KEYWORDS='seo基础入门教程，seo入门学习，seo优化教程'
 WEB_DESCRIPTION='本站长期发布有关seo相关的基础学习教程，包括seo建站流程、seo常见问题，seo日常维护等；另外也会开发和发布一些建站工具！是各建站新手和seo新手的学习之地。'
