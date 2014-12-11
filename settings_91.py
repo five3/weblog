2104bb92ba27d9e2f93f6d5b135bd1fd00b551b7
Xiaowu Chen <114638571@qq.com>
2014-12-11 14:42:25 +0800

    无

--- settings_91.py
+++ settings_91.py
@@ -3,12 +3,12 @@
 import os
 #网站信息
 WEB_URL='/'
-WEB_NAME='上帝De助手 的博客'
-WEB_SUBNAME='我只生产内容，我不是互联网的搬运工！'
-WEB_TITLE='seo基础入门教程_seo入门学习_seo优化教程'
-WEB_KEYWORDS='seo基础入门教程，seo入门学习，seo优化教程'
-WEB_DESCRIPTION='本站长期发布有关seo相关的基础学习教程，包括seo建站流程、seo常见问题，seo日常维护等；另外也会开发和发布一些建站工具！是各建站新手和seo新手的学习之地。'
-TEMPLATE_THEME='default'
+WEB_NAME=' 91小象网，发现优质产品！'
+WEB_SUBNAME='让送礼从此轻松又愉快！'
+WEB_TITLE='好礼物推荐_什么值得买_创意礼品店'
+WEB_KEYWORDS='好礼物推荐，值得推荐的产品，什么值得买，创意礼品店，创意生日礼物'
+WEB_DESCRIPTION='专注于学生、白领等青年人群的送礼导购网站，提供增值的在线许愿服务。'
+TEMPLATE_THEME='xiaoxiang'
 PER_PAGE_COUNT = 10
 
 #账号相关
@@ -35,5 +35,5 @@ ADMIN_TPL_DIR='admin'
 # DB_STRING=DATA_DIR_PATH+'cms.db'
 # DB_TABEL_PREFIX='cms_'
 DB_TYPE='mysql'
-DB_STRING='localhost/3306/root/changeit!/weblog'
+DB_STRING='localhost/3306/root/changeit!/xiaoxiang'
 DB_TABEL_PREFIX='cms_'
